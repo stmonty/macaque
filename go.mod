9b9e1983cc33a66dd438ed3348dd07cee7c0b105
stmonty <stmontydev@gmail.com>
2022-01-21 20:15:12 -0600

    Started working on lexer

---
+++ go.mod
@@ -0,0 +1,3 @@
+module macaque
+
+go 1.17
---
+++ token/token.go
@@ -0,0 +1,37 @@
+package token
+
+type TokenType string
+
+type Token struct {
+    Type    TokenType
+    Literal string
+
+}
+
+const (
+
+    ILLEGAL = "ILLEGAL"
+    EOF     = "EOF"
+
+    //Identifiers and Literals
+    IDENT = "IDENT"
+    INT   = "INT"
+
+    // Operators
+    ASSIGN = "="
+    PLUS   = "+"
+
+
+    //Delimiters
+    SEMICOLON = ";"
+    COMMA     = ","
+    LPAREN    = "("
+    RPAREN    = ")"
+    LBRACE    = "{"
+    RBRACE    = "}"
+
+    //Keywords
+    FUNCTION = "FUNC"
+    LET      = "LET"
+
+)
