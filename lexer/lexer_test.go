261d68848269782f956a3d438767b3a16e7e19a0
stmonty <stmontydev@gmail.com>
2022-01-24 17:01:13 -0600

    minimal test pass

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -9,12 +9,12 @@ import (
 
 func TestNextToken(t *testing.T) {
 
-    input := "let five = 5;
+    input := `let five = 5;
     let ten = 10;
     let add = fn(x, y) {
         x + y;
     };
-    let result = add(five, ten);"
+    let result = add(five, ten);`
 
     tests := []struct {
         expectedType token.TokenType
